9705bf47756c63b7464826c9ff67958766fd1449
Melvin2204 <26822175+melvin2204@users.noreply.github.com>
2018-04-30 17:33:08 +0200

    Added a header + optimised code

--- conf/conf.py
+++ conf/conf.py
@@ -2,7 +2,7 @@ import logging
 import json
 import mimes
 HOSTNAME = "192.168.178.50"
-PORT = 81
+PORT = 8080
 PUBLIC_DIR = "public" # folder to load documents from
 ERROR_DOC = { #Location of error documents. Loaded from root folder
     "404":"404.html"
